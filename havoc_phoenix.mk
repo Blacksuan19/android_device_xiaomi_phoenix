c1736981fdcbe6cedee8ef8917f00c951214d397
blacksuan19 <i@blacksuan19.me>
2021-05-26 00:37:26 +0800

    phoenix: bring up havoc
    i need to stop changing trees on each build
    
    Signed-off-by: blacksuan19 <i@blacksuan19.me>

--- havoc_phoenix.mk
+++ havoc_phoenix.mk
@@ -6,19 +6,18 @@
 
 $(call inherit-product, device/xiaomi/phoenix/device.mk)
 
-# Inherit some common ArrowOS stuff.
-$(call inherit-product, vendor/arrow/config/common.mk)
+# Inherit some common havocOS stuff.
+$(call inherit-product, vendor/havoc/config/common.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := arrow_phoenix
+PRODUCT_NAME := havoc_phoenix
 PRODUCT_DEVICE := phoenix
 PRODUCT_BRAND := POCO
 PRODUCT_MODEL := POCO X2
 PRODUCT_MANUFACTURER := Xiaomi
 
-# ArrowOS Properties
+# havocOS Properties
 TARGET_GAPPS_ARCH := arm64
 TARGET_BOOT_ANIMATION_RES := 1080
-DEVICE_MAINTAINER := Sreekanth
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
